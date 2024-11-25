9eafcbb39ce4e9f390e6c21700f1db3d1a4abcc9
Dhruv Khandelwal <artificialintelligencehub35@gmail.com>
2024-11-25 21:03:42 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import React, { useRef, useState, useEffect } from 'react'
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
